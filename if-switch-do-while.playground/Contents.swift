e9fe9ee675bf6367f709d98b451de338ff9116c6
Cem Yurtdas <67091460+Cem2204@users.noreply.github.com>
2023-07-20 22:38:47 +0300

    Add files via upload

---
+++ if-switch-do-while.playground/Contents.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+// True False
+/*
+var s1 = 60
+var s2 = 50
+
+var y1 = 70
+var y2 = 79
+
+print(s1 == s2)
+print(s1 > s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 <= s2)
+print(s1 != s2)
+
+print(s1 > s2 || y1 > y2)
+print(s1 > s2 && y1 > y2)
+
+//If - else
+
+var varA:Int = 10
+var yas = 17
+var isim = "Ahmet"
+
+if yas > 18 {
+    print("Reşitsiniz")
+}
+else{
+    print("Reşit değilsiniz")
+}
+
+
+if(varA < 20){
+    print("varA 20'den küçük")
+}
+else{
+    print("varA 20'den büyük")
+}
+
+if isim == "Ahmet"{
+    print("Merhaba Ahmet")
+}
+else {
+    print("Tanınmayan kişi")
+}
+
+// Ternary Conditional
+
+var a = 10
+var b = 20
+
+a == b ? print("Eşit") : print("Eşit Değil")
+
+
+
+var secim = 2
+
+var kisaKenar = 10
+var uzunKenar = 20
+var yariCap = 4
+
+print("Seçiminiz : \(secim)")
+
+if(secim == 2){
+    print("Dikdörtgen alanı : \(kisaKenar*uzunKenar)")
+}
+else if(secim == 1){
+    print("Karenin alanı :")
+    print(kisaKenar*kisaKenar)
+}
+
+// Switch Case
+ 
+ 
+var gun = 3
+
+switch gun {
+    
+case 1:
+    print("Pazartesi")
+case 2:
+    print("Salı")
+case 3:
+    print("Çarşamba")
+case 4:
+    print("Perşembe")
+case 5:
+    print("Cuma")
+case 6:
+    print("Cumartesi")
+case 7:
+    print("Pazar")
+default:
+    print("Böyle bir gün yok")
+    
+}
+
+
+var sayi = 5
+var sonuc = sayi % 2
+switch sonuc {
+    
+case 0:
+    print("Sayımız çift")
+case 1:
+    print("Sayımız tek")
+default:
+    print("Hata....")
+    
+}
+
+var dizi:[Int] = [10,20,30]
+
+for i in 1...50{
+    print(i)
+}
+
+var baslangic = 10
+var bitis = 20
+var artis = 5
+
+for a in stride(from: baslangic, through: bitis, by: artis){
+    print(a)
+}
+
+
+var sayac = 0
+
+while sayac < 10 {
+    print(sayac)
+    sayac+=1
+}
+*/
+
+for i in stride(from: 0, through: 8, by: 2){
+    print(i)
+}
